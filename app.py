b1c3f845d03ba53f0c4d0f3decb054b579c3a8b9
slavaincos <slavaincos@gmail.com>
2024-04-13 07:47:08 +0300

    constants in file

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
 import telebot
 from config import TOKEN
-import requests
 from extensions import ExchangeRate
-
-with open('config.txt', 'r') as f:
-    data = f.read().splitlines()
-EXCH = data[0]
-TOKEN = data[1] 
     
     
-
-print(ExchangeRate.rate('USD', 'RUB'))
+print(ExchangeRate.rate(['доллар', 'рубль', 1]))
 
 
 # bot = telebot.TeleBot(TOKEN)
--- config.py
+++ config.py
@@ -3,3 +3,8 @@ keys = {
     'доллар': 'USD',
     'рубль': 'RUB',        
 }
+
+with open('config.txt', 'r') as f:
+    data = f.read().splitlines()
+EXCH = data[0].replace('EXCH = ', '')
+TOKEN = data[1].replace('TOKEN = ', '')
