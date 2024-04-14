5df7f0441adb18224c730f898d621ab3b37f6da6
slavaincos <slavaincos@gmail.com>
2024-04-14 06:54:57 +0300

    ready for checking

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 import telebot
-from config import TOKEN
-from extensions import ExchangeRate
+from config import TOKEN, keys
+from extensions import ExchangeRate, APIExeption
     
     
-print(ExchangeRate.rate(['доллар', 'рубль', 1]))
+# print(ExchangeRate.rate(['доллар', 'рубль', 1]))
 
+bot = telebot.TeleBot(TOKEN)
 
-# bot = telebot.TeleBot(TOKEN)
-# bot.polling(none_stop=True) #run bot, don't stop error appearing
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	txt = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
+<в какую валюту перевести> \
+<количество переводимой валюты>'
+	bot.reply_to(message, txt)
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-# 	bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=['values'])
+def values(message):
+	txt = 'Доступные валюты:'
+	for key in keys:
+		txt = '\n'.join((txt, key, ))
+	bot.reply_to(message, txt)
 
+@bot.message_handler(content_types=['text',])
+def convert(message):
+	try:
+		values = message.text.split(' ')
+		if len(values) != 3:
+			raise APIExeption('Количество параметров при конвертации должно быть 3')
+		result = ExchangeRate.get_price(values)
+	except APIExeption as e:
+		bot.send_message(message.chat.id, f'Ошибка пользователя\n{e}')
+	except Exception as e:
+		bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+	else:
+		bot.send_message(message.chat.id, result)
+
+bot.polling() #run bot, don't stop error appearing none_stop=True
--- extensions.py
+++ extensions.py
@@ -3,52 +3,35 @@ from config import EXCH, keys
 
 class ExchangeRate():
   @staticmethod
-  def rate(*args):
+  def get_price(*args):
     data = args[0]
     try:
       base = keys[data[0]]
     except KeyError:
-      APIExeption(f'Неверно указана базовая валюта {data[0]}')
-      return  
+      raise APIExeption(f'Неверно указана базовая валюта {data[0]}')
 
     try:
       quote = keys[data[1]]
     except KeyError:
-      APIExeption(f'Неверно указана расчетная валюта {data[1]}')
-      return 
+      raise APIExeption(f'Неверно указана расчетная валюта {data[1]}')
     
     try:
       amount = float(data[2])
     except ValueError:
-      APIExeption(f'Неверное значение количества {data[2]}')
-      return
+      raise APIExeption(f'Неверное значение количества {data[2]}')
     
     if base == quote:
-      raise APIExeption(f'Невозможно конвертировать одинаковые валюты {base}')    
-      return
+        raise APIExeption(f'Невозможно конвертировать одинаковые валюты {data[0]}')  
     
-    print('Успешно конвертировано!')  
+    # print('Успешно конвертировано!')  
       
     url = 'https://v6.exchangerate-api.com/v6/'+ EXCH + '/latest/' + base
     response = requests.get(url)
-    data = response.json()
-    return data['conversion_rates'][quote] * amount
+    conv = response.json()
+    result = conv['conversion_rates'][quote] * amount
+    
+    return f'цена {amount} {data[0]} в {data[1]} - {result}'
   
 class APIExeption(Exception):
-  def __init__(self, *args: object) -> None:
-    super().__init__(*args)
-    print(args[0])
-    
-if __name__ == '__main__':
-  while True:
-    query = input('Введите запрос на конвертацию: ')    
-    values = query.split(' ')
-    if values[0] == 'quit':
-      break
-    if len(values) != 3:
-      try:
-        raise APIExeption('Количество параметров при конвертации должно быть 3')
-      except:
-        continue
+  pass
 
-    ExchangeRate.rate(values)
